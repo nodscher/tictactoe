5b4126ef790e7ca74285e9d6e081f503b9aa3d40
Robert Repnak <robert.repnak@slice-dice.de>
2015-10-18 14:42:47 +0200

    fixed winner dialog

--- tic tac toe/GameViewController.swift
+++ tic tac toe/GameViewController.swift
@@ -47,13 +47,22 @@ class GameViewController: UIViewController {
         }
         if (isWinner(1))
         {
-            let alertView = UIAlertController(title: "Default Style", message: "A standard alert.",preferredStyle: .Alert)
+            let alertView = UIAlertController(title: "Herzlichen Glückwunsch", message: "Spieler 1 hat gewonnen!",preferredStyle: .Alert)
+            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+                // ...
+            }
+            alertView.addAction(OKAction)
             self.presentViewController(alertView, animated: true, completion: nil)
 
             
         } else if (isWinner(2))
         {
-            
+            let alertView = UIAlertController(title: "Herzlichen Glückwunsch", message: "Spieler 2 hat gewonnen!",preferredStyle: .Alert)
+            let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
+                // ...
+            }
+            alertView.addAction(OKAction)
+            self.presentViewController(alertView, animated: true, completion: nil)
         }
         
     }
@@ -77,61 +86,47 @@ class GameViewController: UIViewController {
         
     }
     
-    @IBAction func setButtonTextColorToRed(buttonID : NSInteger) {
-        let button :UIButton = self.view.viewWithTag(1000+buttonID) as! UIButton;
-        button.setTitleColor(UIColor.redColor(), forState: .Normal);
+    func setButtonTextColorToRed(buttonIDs : [NSInteger]) {
+        for buttonID in buttonIDs
+        {
+            let button :UIButton = self.view.viewWithTag(1000+buttonID) as! UIButton;
+            button.setTitleColor(UIColor.redColor(), forState: .Normal);
+        }
     }
     
     func isWinner(user:NSInteger) -> Bool
     {
         if ((selectedArray[0] == user) && (selectedArray[3] == user) && (selectedArray[6] == user))
         {
-            setButtonTextColorToRed(0);
-            setButtonTextColorToRed(3);
-            setButtonTextColorToRed(6);
-            
+            setButtonTextColorToRed([0,3,6]);
             return true;
         } else if ((selectedArray[1] == user) && (selectedArray[4] == user) && (selectedArray[7] == user))
         {
-            setButtonTextColorToRed(1);
-            setButtonTextColorToRed(4);
-            setButtonTextColorToRed(7);
+            setButtonTextColorToRed([1,4,7]);
             return true;
         } else if ((selectedArray[2] == user) && (selectedArray[5] == user) && (selectedArray[8] == user))
         {
-            setButtonTextColorToRed(2);
-            setButtonTextColorToRed(5);
-            setButtonTextColorToRed(8);
+            setButtonTextColorToRed([2,5,8]);
             return true;
         } else if ((selectedArray[0] == user) && (selectedArray[1] == user) && (selectedArray[2] == user))
         {
-            setButtonTextColorToRed(0);
-            setButtonTextColorToRed(1);
-            setButtonTextColorToRed(2);
+            setButtonTextColorToRed([0,1,2]);
             return true;
         } else if ((selectedArray[3] == user) && (selectedArray[4] == user) && (selectedArray[5] == user))
         {
-            setButtonTextColorToRed(3);
-            setButtonTextColorToRed(4);
-            setButtonTextColorToRed(5);
+            setButtonTextColorToRed([3,4,5]);
             return true;
         } else if ((selectedArray[6] == user) && (selectedArray[7] == user) && (selectedArray[8] == user))
         {
-            setButtonTextColorToRed(6);
-            setButtonTextColorToRed(7);
-            setButtonTextColorToRed(8);
+            setButtonTextColorToRed([6,7,8]);
             return true;
         } else if ((selectedArray[0] == user) && (selectedArray[4] == user) && (selectedArray[8] == user))
         {
-            setButtonTextColorToRed(0);
-            setButtonTextColorToRed(4);
-            setButtonTextColorToRed(8);
+            setButtonTextColorToRed([0,4,8]);
             return true;
         } else if ((selectedArray[2] == user) && (selectedArray[4] == user) && (selectedArray[6] == user))
         {
-            setButtonTextColorToRed(2);
-            setButtonTextColorToRed(4);
-            setButtonTextColorToRed(6);
+            setButtonTextColorToRed([2,4,6]);
             return true;
         }
         
